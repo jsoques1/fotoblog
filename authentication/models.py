03fdb0421636b8f531378ced7093b641b4f0fd34
jsoques1 <joseph.soques@gmail.com>
2022-05-12 17:37:44 +0200

    initial commit

--- authentication/models.py
+++ authentication/models.py
@@ -1,25 +1,3 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
 
-
-class User(AbstractUser):
-    CREATOR = 'CREATOR'
-    SUBSCRIBER = 'SUBSCRIBER'
-
-
-    ROLE_CHOICES = (
-        (CREATOR, 'Créateur'),
-        (SUBSCRIBER, 'Abonné'),
-    )
-
-    profile_photo = models.ImageField(verbose_name='Photo de profil')
-    role = models.CharField(max_length=30, choices=ROLE_CHOICES, verbose_name='Rôle')
-
-    def save(self, *args, **kwargs):
-        super().save(*args, **kwargs)
-        if self.role == self.CREATOR:
-            group = Group.objects.get(name='creators')
-            group.user_set.add(self)
-        elif self.role == self.SUBSCRIBER:
-            group = Group.objects.get(name='subscribers')
-            group.user_set.add(self)
+# Create your models here.
