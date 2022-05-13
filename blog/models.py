e19b00d25e8e8d0e650b96a9ddd7f9fede65354b
jsoques1 <joseph.soques@gmail.com>
2022-05-13 15:35:44 +0200

    permissions simples

--- blog/models.py
+++ blog/models.py
@@ -1,7 +1,6 @@
-from django.db import models
-
 from django.conf import settings
 from django.db import models
+from PIL import Image
 
 
 class Photo(models.Model):
@@ -9,6 +8,19 @@ class Photo(models.Model):
     caption = models.CharField(max_length=128, blank=True)
     uploader = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     date_created = models.DateTimeField(auto_now_add=True)
+
+    IMAGE_MAX_SIZE = (800, 800)
+
+    def resize_image(self):
+        image = Image.open(self.image)
+        image.thumbnail(self.IMAGE_MAX_SIZE)
+        # sauvegarde de l’image redimensionnée dans le système de fichiers
+        # ce n’est pas la méthode save() du modèle !
+        image.save(self.image.path)
+
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+        self.resize_image()
 
 
 class Blog(models.Model):
